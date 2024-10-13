32ea8955d8a1ccdc672be4ac8ff3ceac34b3540b
Mohamed Saied <mohamed.said123738@gmail.com>
2024-10-13 20:26:52 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_forecasting/api/firebase_api.dart';
 import 'package:weather_forecasting/weather_info.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
